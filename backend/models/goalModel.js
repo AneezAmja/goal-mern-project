694d150bfab508ca93dd5876067498633669e18f
AneezAmja <aneez.amja@gmail.com>
2023-06-30 01:02:18 +0100

    GoalModel: add: linked User model

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 const goalSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId, // Getting the _id generated from Mongodb
+        required: true,
+        ref: 'User' // Allows us to know what this ObjectId relates too i.e. the User
+    },
     text: {
         type: String,
         required:[true, 'please add a text value']
